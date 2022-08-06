aa76e5a48123f790984b761713bc895d07a0ca71
izzumani <isaac.njiru@gmail.com#>
2022-08-06 04:31:31 +0300

    update

--- EFCore_Activity0201/Program.cs
+++ EFCore_Activity0201/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_DBLibrary;
+using EFCore_DBLibrary.DTOs;
 using Inventory.Common.LoggerBuilder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +23,7 @@ public class Program
         _logger.Debug($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
         BuildOptions();
         //ListPeople();
+
         _logger.Debug("List People Then Order and Take");
         ListPeopleThenOrderAndTake();
         _logger.Debug("Query People, order, then list and take");
@@ -50,6 +52,10 @@ public class Program
             Console.WriteLine($"Page {pageNumber + 1}");
             FilteredAndPagedResult("VC", pageNumber, pageSize);
         }
+
+        ListAllSalespeople();
+        /**/
+        GenerateSalesReportData();
     }
 
     static void BuildConfiguration()
@@ -138,7 +144,99 @@ public class Program
         }
     }
 
+    private static void ListAllSalespeople()
+    {
+        using (var db = new AdventureWorks2019Context(_optionsBuilder.Options))
+        {
+            var salespeople = db.SalesPeople
+                                //.Include(x => x.BusinessEntity)
+                                //.ThenInclude(y => y.BusinessEntity)
+                                .AsNoTracking()
+                                .Select(x=> new { 
+                                x.BusinessEntityId,
+                                x.TerritoryId,
+                                x.SalesQuota,
+                                x.Bonus,
+                                x.SalesYtd,
+                                x.BusinessEntity.BusinessEntity.FirstName,
+                                x.BusinessEntity.BusinessEntity.LastName,
+                                })
+                                .ToList();
+            foreach (var salesperson in salespeople)
+            {
+                ///var p = db.People.FirstOrDefault(x => x.BusinessEntityId == salesperson.BusinessEntityId);
+                ///_logger.Debug(GetSalespersonDetail(salesperson, p));
+                //_logger.Debug($"ID: {salesperson.BusinessEntityId}\t|TID: {salesperson.TerritoryId}\t|Quota:{salesperson.SalesQuota}\t" + $"|Bonus: {salesperson.Bonus}\t|YTDSales: {salesperson.SalesYtd}\t|Name: \t" + $"{p?.FirstName ?? ""}, {p?.LastName ?? ""}");
+                //_logger.Debug($"ID: {salesperson.BusinessEntityId}\t|TID: {salesperson.TerritoryId}\t|Quota:{salesperson.SalesQuota}\t" + $"|Bonus: {salesperson.Bonus}\t|YTDSales: {salesperson.SalesYtd}\t|Name: \t" + $"{salesperson?.BusinessEntity?.BusinessEntity?.FirstName ?? ""}, {salesperson?.BusinessEntity?.BusinessEntity?.LastName ?? ""}");
+                _logger.Debug($"ID: {salesperson.BusinessEntityId}\t|TID: {salesperson.TerritoryId}\t|Quota:{salesperson.SalesQuota}\t" + $"|Bonus: {salesperson.Bonus}\t|YTDSales: {salesperson.SalesYtd}\t|Name: \t" + $"{salesperson?.FirstName ?? ""}, {salesperson?.LastName ?? ""}");
+            }
+        }
+    }
 
+
+    private static void GenerateSalesReportData()
+    {
+        using (var db = new AdventureWorks2019Context(_optionsBuilder.Options))
+        {
+            var salesReportData = db.SalesPeople.Select(sp => new
+            {
+                beid = sp.BusinessEntityId,
+                sp.BusinessEntity.BusinessEntity.FirstName,
+                sp.BusinessEntity.BusinessEntity.LastName,
+                sp.SalesYtd,
+                Territories = sp.SalesTerritoryHistories.Select(y => y.Territory.Name), 
+                OrderCount = sp.SalesOrderHeaders.Count(),
+                TotalProductsSold = sp.SalesOrderHeaders
+                                        .SelectMany(y => y.SalesOrderDetails)
+                                        .Sum(z => z.OrderQty)
+            }).Where(srdata => srdata.SalesYtd > 3000000)
+                .OrderBy(srds => srds.LastName)
+                .ThenBy(srds => srds.FirstName)
+                .ThenByDescending(srds => srds.SalesYtd)
+                .ToList();
+            foreach (var srd in salesReportData)
+            {
+                _logger.Debug($"{srd.beid}| {srd.LastName}, {srd.FirstName}|" 
+                    + $"YTD Sales: {srd.SalesYtd} |" 
+                    + $"{string.Join(',', srd.Territories)}"
+                    + $"Order Count: {srd.OrderCount}"
+                    + $"Products Sold: {srd.TotalProductsSold}"
+                    );
+            }
+        }
+    }
+
+    private static void GenerateSalesReportDataToDTO()
+    {
+        using (var db = new AdventureWorks2019Context(_optionsBuilder.Options))
+        {
+            var salesReportData = db.SalesPeople.Select(sp => new SalesReportListingDto
+            {
+                BusinessEntityId = sp.BusinessEntityId,
+                FirstName = sp.BusinessEntity.BusinessEntity.FirstName,
+                LastName = sp.BusinessEntity.BusinessEntity.LastName,
+                SalesYtd = sp.SalesYtd,
+                Territories = sp.SalesTerritoryHistories.Select(y => y.Territory.Name),
+                TotalOrders = sp.SalesOrderHeaders.Count(),
+                TotalProductsSold = sp.SalesOrderHeaders
+                                        .SelectMany(y => y.SalesOrderDetails)
+                                        .Sum(z => z.OrderQty)
+            }).Where(srdata => srdata.SalesYtd > 3000000)
+                .OrderBy(srds => srds.LastName)
+                .ThenBy(srds => srds.FirstName)
+                .ThenByDescending(srds => srds.SalesYtd);
+               // .ToList();
+            foreach (var srd in salesReportData)
+            {
+                _logger.Debug($"{srd.ToString()}");
+                   
+            }
+        }
+    }
+    private static string GetSalespersonDetail(SalesPerson sp, Person p)
+    {
+        return $"ID: {sp.BusinessEntityId}\t|TID: {sp.TerritoryId}\t|Quota:{ sp.SalesQuota}\t" + $"|Bonus: {sp.Bonus}\t|YTDSales: {sp.SalesYtd}\t|Name: \t" +$"{p?.FirstName ?? ""}, {p?.LastName ?? ""}";
+    }
 
 }
 
